325e5cce5f588fbc17464aa26a881e0da75afac8
Spandana825 <spandanasreddy528@gmail.com>
2024-02-14 13:36:20 +0530

    second

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent=React.createElement("div",{id:"parent"},
+                                React.createElement("div",{id:"child"},
+                                [React.createElement("h1",{id:"heading"},"hi i am  first h1 tag"),
+                                React.createElement("h1",{id:"heading"},"hi i am  second h1 tag")]
+                                ));
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
