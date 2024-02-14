e6f772ee6f2f40fd82a02904aa3779941dfe784f
Spandana825 <spandanasreddy528@gmail.com>
2024-02-14 17:46:38 +0530

    heading

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import ReactDOM from "react-dom/client";
 const parent=React.createElement("div",{id:"parent"},
                                 React.createElement("div",{id:"child"},
                                 [React.createElement("h1",{id:"heading"},"hi i am  first h1 tag"),
-                                React.createElement("h1",{id:"heading"},"hi i am  second h1 tag")]
+                                React.createElement("h1",{id:"heading"},"hi i am  second h1 tag"),
+                                React.createElement("h2",{id:"heading"},"hi i am third h1 tag")]
                                 ));
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
